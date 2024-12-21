641b7d90854fd533a6feebeda5ce013927a6e96f
Evgeny Lim <evgeny.lim4@gmail.com>
2024-12-22 01:52:50 +0300

    4. Тесты для стороны прямоугольника. [#159460 #74294]

--- src/main/java/ru/job4j/condition/RectangleArea.java
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -9,7 +9,13 @@ public class RectangleArea {
     }
 
     public static void main(String[] args) {
-        double result = RectangleArea.square(6, 2);
-        System.out.println(" p = 6, k = 2, s = 2, actual s result = " + result);
+        double result1 = RectangleArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, actual s result = " + result1);
+        double result2 = RectangleArea.square(5, 4);
+        System.out.println(" p = 5, k = 4, s = 2, actual s result = " + result2);
+        double result3 = RectangleArea.square(2, 2);
+        System.out.println(" p = 2, k = 2, s = 2, actual s result = " + result3);
+        double result4 = RectangleArea.square(4, 4);
+        System.out.println(" p = 4, k = 4, s = 2, actual s result = " + result4);
     }
 }
---
+++ src/test/java/ru/job4j/condition/RectangleAreaTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class RectangleAreaTest {
+
+    @Test
+    void whenP6K2Square2Then2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double output = RectangleArea.square(p, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP5K4Square2Then1() {
+        int expected = 1;
+        int p = 5;
+        double k = 4;
+        double output = RectangleArea.square(p, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP2K2Square2Then0Dot22() {
+        double expected = 0.22;
+        int p = 2;
+        double k = 2;
+        double output = RectangleArea.square(p, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP4K4Square2Then0Dot64() {
+        double expected = 0.64;
+        int p = 4;
+        double k = 4;
+        double output = RectangleArea.square(p, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+}
